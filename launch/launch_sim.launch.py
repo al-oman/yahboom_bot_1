98ab9510d6c392314ce92e19630018660365cc2c
Alex <al_oman@live.concordia.ca>
2025-05-23 01:05:15 -0400

    added depth camera functionality, continued troubleshooting inertia/PID issues of yahboom xacro with real inertia values in robot_core_TEST

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,9 +2,10 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
+from launch.substitutions import LaunchConfiguration
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -12,7 +13,21 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    
+    log_level_arg = DeclareLaunchArgument(
+        'log_level',
+        default_value='debug',
+        description='Logging level (debug, info, warn, error, fatal)'
+    )
 
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true'
+    )
+
+    log_level = LaunchConfiguration('log_level')
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
@@ -22,7 +37,7 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': use_sim_time}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
@@ -34,12 +49,15 @@ def generate_launch_description():
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'my_bot'],
+                                   '-entity', 'my_bot',
+                                   '--ros-args', '--log-level', log_level],
                         output='screen')
 
 
     # Launch them all!
     return LaunchDescription([
+        log_level_arg,
+        use_sim_time_arg,
         rsp,
         gazebo,
         spawn_entity,
