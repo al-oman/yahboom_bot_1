1e3698bf972834a5b158c1af2b40fb32d0385265
Alex <al_oman@live.concordia.ca>
2025-06-18 13:31:47 -0400

    incorporating ros_control instead of just gazebo control plugins, expanded launch file

---
+++ launch/rolling.launch.py
@@ -0,0 +1,75 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch.substitutions import LaunchConfiguration
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+    
+    log_level_arg = DeclareLaunchArgument(
+        'log_level',
+        default_value='debug',
+        description='Logging level (debug, info, warn, error, fatal)'
+    )
+
+    use_sim_time_arg = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (Gazebo) clock if true'
+    )
+
+    log_level = LaunchConfiguration('log_level')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+
+    package_name='yahboom_bot_1' #<--- CHANGE ME
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': use_sim_time}.items()
+    )
+
+    # Include the Gazebo launch file, provided by the gazebo_ros package
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+                launch_arguments={'world': os.path.join(get_package_share_directory(package_name), 'worlds', 'obstacles.world')}.items()
+             )
+
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot',
+                                   '--ros-args', '--log-level', log_level],
+                        output='screen')
+    
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', os.path.join(get_package_share_directory(package_name), 'config', 'rviz_config.rviz')],
+        parameters=[{'use_sim_time': use_sim_time}]
+    )
+
+    # Launch them all!
+    return LaunchDescription([
+        log_level_arg,
+        use_sim_time_arg,
+        rsp,
+        gazebo,
+        spawn_entity,
+        rviz_node
+    ])
